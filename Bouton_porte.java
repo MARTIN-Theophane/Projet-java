76f9ad57feb0e53a7c5001ad8d55814152b10cc4
MARTIN-Theophane <2007martintheophane@gmail.com>
2026-04-28 14:54:59 +0200

    Amélioration Bouton_porte.java

--- Bouton_porte.java
+++ Bouton_porte.java
@@ -1,9 +1,13 @@
 public class Bouton_porte {
-	int num_bouton_porte;
-	boolean etat;
-	Porte porte;
+	private int num_bouton_porte;
+	private Porte porte;
 	
-	void appuyerBoutonPorte() {
-		etat = !etat;
+	public Bouton_porte(int num_bouton_porte, Porte porte) {
+		this.num_bouton_porte = num_bouton_porte;
+		this.porte = porte;
+	}
+
+	boolean appuyerBoutonPorte() {
+		return porte.fermerPorte();
 	}
 }
