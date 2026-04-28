91eb85ab3059fd7644cf27c388c7398266583c45
MARTIN-Theophane <2007martintheophane@gmail.com>
2026-04-28 14:55:51 +0200

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -5,9 +5,21 @@ import javax.swing.SwingUtilities;
 public class Joueur extends JFrame {
 	int regard = 2; //Définit où le joueur regarde
 	
+	//Création des lumieres et de leurs boutons
+	private Lumière lumiere_gauche = new Lumière(1,false);
+	private Lumière lumiere_droite = new Lumière(2,false);
+	Bouton_lumiere bouton_lumiere_gauche = new Bouton_lumiere(1,lumiere_gauche);
+	Bouton_lumiere bouton_lumiere_droite = new Bouton_lumiere(2,lumiere_droite);
+	
+	//Création des portes et de leurs boutons
+	private Porte porte_gauche = new Porte(1,false);
+	private Porte porte_droite = new Porte(2,false);
+	Bouton_porte bouton_porte_gauche = new Bouton_porte(1,porte_gauche);
+	Bouton_porte bouton_porte_droite = new Bouton_porte(1,porte_droite);
+	
 	public Joueur() {
 		//Fenêtre du jeu
-		setTitle("Détection de touches spécifiques");
+		setTitle("Projet java");
 		setSize(400, 250);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocation(-2000,-2000);
@@ -26,10 +38,10 @@ public class Joueur extends JFrame {
 		System.out.println("Le jeu est lancé.");
 	}
 	
-	//Méthode permettant l'action entraainé par l'appui d'une touche
+	//Méthode permettant l'action entrainé par l'appui d'une touche
 	private void ActionTouches(KeyEvent e) {
 		switch (e.getKeyCode()) {
-		case KeyEvent.VK_Q:
+		case KeyEvent.VK_Q: //Regarder plus à droite
 			if (this.regard != 1) {
 				this.regard -= 1;
 			}
@@ -39,7 +51,7 @@ public class Joueur extends JFrame {
 			case 3 -> System.out.println("Vous regardez à droite de vous.");
 			}
 			break;
-		case KeyEvent.VK_D:
+		case KeyEvent.VK_D: //Regarder plus à gauche
 			if (this.regard != 3) {
 				this.regard += 1;
 			}
@@ -49,19 +61,36 @@ public class Joueur extends JFrame {
 			case 3 -> System.out.println("Vous regardez à droite de vous.");
 			}
 			break;
-		case KeyEvent.VK_A:
-			System.out.println("Vous avez allumer ou éteint la lumière de gauche.");
+		case KeyEvent.VK_A: //Lumière gauche
+			if (this.bouton_lumiere_gauche.appuyerBoutonLumiere() == false) {
+				System.out.println("Vous avez éteint la lumière de gauche.");
+			} else {
+				System.out.println("Vous avez allumé la lumière de gauche.");
+			}
 			break;
-		case KeyEvent.VK_E:
-			System.out.println("Vous avez allumer ou éteint la lumière de droite.");
+		case KeyEvent.VK_E: //Lumière droite
+			if (this.bouton_lumiere_droite.appuyerBoutonLumiere() == false) {
+				System.out.println("Vous avez éteint la lumière de droite.");
+			} else {
+				System.out.println("Vous avez allumé la lumière de droite.");
+			}
 			break;
-		case KeyEvent.VK_W:
-			System.out.println("Vous avez ouvert ou fermer la porte de gauche.");
+		case KeyEvent.VK_W: //Porte gauche
+			if (this.bouton_porte_gauche.appuyerBoutonPorte() == false) {
+				System.out.println("Vous avez ouvert la porte de gauche.");
+			} else {
+				System.out.println("Vous avez fermé la porte de gauche.");
+			}
 			break;
-		case KeyEvent.VK_C:
-			System.out.println("Vous avez ouvert ou fermer la porte de droite.");
+		case KeyEvent.VK_C: //Porte droite
+			if (this.bouton_porte_droite.appuyerBoutonPorte() == false) {
+				System.out.println("Vous avez ouvert la porte de droite.");
+			} else {
+				System.out.println("Vous avez fermé la porte de droite.");
+			}
 			break;
-		case KeyEvent.VK_F1:
+		case KeyEvent.VK_F1: //Liste des touches
+			System.out.println();
 			System.out.println("Liste des touches");
 			System.out.println("Regarder vers la gauche : Q");
 			System.out.println("Regarder vers la droite : D");
